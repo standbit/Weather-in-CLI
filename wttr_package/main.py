17bd4849f140176a8adf1c350502e7011ee9d1a6
Stanislav <yastva3@gmail.com>
2022-01-06 13:12:19 +0300

    Put GET params into separate dict

--- wttr_package/main.py
+++ wttr_package/main.py
@@ -2,16 +2,22 @@ import requests
 
 
 URL_TEMPLATE = "http://wttr.in/{}"
+
 LOCATIONS = [
-    "Лондон?nTqm&lang=ru",
-    "Шереметьево?nTqm&lang=ru",
-    "Череповец?nTqm&lang=ru"]
+    "Лондон",
+    "Шереметьево",
+    "Череповец"]
+
+PAYLOAD = {
+    "nTqm": "",
+    "lang": "ru"
+}
 
 
 def get_response():
     for item in LOCATIONS:
         url = URL_TEMPLATE.format(item)
-        response = requests.get(url)
+        response = requests.get(url, params=PAYLOAD)
         print(response.text)
 
 
